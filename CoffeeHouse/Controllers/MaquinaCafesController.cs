d66560ef39efdb2736d6e353a967ef62f0c0dac6
Karolina Gomes <63608020+karolinagb@users.noreply.github.com>
2020-10-28 20:52:44 -0300

    Implementando construtor

--- CoffeeHouse/Controllers/MaquinaCafesController.cs
+++ CoffeeHouse/Controllers/MaquinaCafesController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoffeeHouse.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoffeeHouse.Controllers
@@ -11,6 +12,48 @@ namespace CoffeeHouse.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Create(MaquinaCafe maquinaCafe)
+        {
+            string descricao = "";
+            double valor = 0.0;
+            ViewBag.Messagem = "";
+
+            switch (maquinaCafe.TipoCafe)
+            {
+                case 1:
+                    descricao = "Cappuccino";
+                    valor = 3.5;
+                    break;
+                case 2:
+                    descricao = "Mocha";
+                    valor = 4;
+                    break;
+                case 3:
+                    descricao = "Café com Leite";
+                    valor = 3;
+                    break;
+                default:
+                    ViewBag.Messagem = "Selecione um dos tipos de cafés disponíveis";
+                    break;
+            }
+
+            var obj = new MaquinaCafe
+            {
+                UmCentavo = maquinaCafe.UmCentavo,
+                CincoCentavo = maquinaCafe.CincoCentavo,
+                DezCentavo = maquinaCafe.DezCentavo,
+                VinteCincoCentavo = maquinaCafe.VinteCincoCentavo,
+                CinquentaCentavo = maquinaCafe.CinquentaCentavo,
+                UmReal = maquinaCafe.UmReal,
+               TipoCafe = maquinaCafe.TipoCafe,
+               Descricao = descricao
+            };
+
+            return View(obj);
         }
     }
 }
--- CoffeeHouse/Models/MaquinaCafe.cs
+++ CoffeeHouse/Models/MaquinaCafe.cs
@@ -1,14 +1,47 @@
-﻿using CoffeeHouse.Models.Enum;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace CoffeeHouse.Models
 {
     public class MaquinaCafe
     {
-        public TipoMoeda TipoMoeda { get; set; }
-        public TipoCafe TipoCafe { get; set; }
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [Range(0,100, ErrorMessage = "Campo {0}: Apenas números positivos")]
+        [Display(Name = "R$ 0,01")]
+        public int UmCentavo { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [Range(0, 100, ErrorMessage = "Campo {0}: Apenas números positivos")]
+        [Display(Name = "R$ 0,05")]
+        public int CincoCentavo { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [Range(0, 100, ErrorMessage = "Campo {0}: Apenas números positivos")]
+        [Display(Name = "R$ 0,10")]
+        public int DezCentavo { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [Range(0, 100, ErrorMessage = "Campo {0}: Apenas números positivos")]
+        [Display(Name = "R$ 0,25")]
+        public int VinteCincoCentavo { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [Range(0, 100, ErrorMessage = "Campo {0}: Apenas números positivos")]
+        [Display(Name = "R$ 0,50")]
+        public int CinquentaCentavo { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [Range(0, 100, ErrorMessage = "Campo {0}: Apenas números positivos")]
+        [Display(Name = "R$ 1,00")]
+        public int UmReal { get; set; }
+
+        [Required(ErrorMessage = "Campo {0} obrigatório")]
+        [Display(Name = "Selecione o café")]
+        public int TipoCafe { get; set; }
+  
+        public string Descricao { get; set; }
+
+        public MaquinaCafe()
+        {
+        }
     }
 }
